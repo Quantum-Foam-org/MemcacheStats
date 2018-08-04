e83c6cf59f8fd31c5b043e3261911f0b565c2dee
Michael Alaimo <malaimo923@gmail.com>
2018-08-03 21:48:00 -0400

    Update to run in phar

--- Stats.php
+++ Stats.php
@@ -1,25 +1,22 @@
 <?php
+$common_php_dir = 'php_common';
+$common_autoload_file = $common_php_dir . '/autoload.php';
+require ($common_autoload_file);
 
-$common_php_dir = '../php_common';
-$common_autoload_file = $common_php_dir.'/autoload.php';
-require($common_autoload_file);
+$cli_php_dir = 'php_cli';
+$cli_autoload_file = $cli_php_dir . '/autoload.php';
+require ($cli_autoload_file);
 
-$cli_php_dir = '../php_cli';
-$cli_autoload_file = $cli_php_dir.'/autoload.php';
-require($cli_autoload_file);
+require ('local/autoload.php');
 
-
-require('./local/autoload.php');
-
-
-use \cli\classes as cli;
-use \local\classes\cli as localCli;
+use cli\classes as cli;
+use local\classes\cli as localCli;
 
 \common\Config::obj(__DIR__ . '/config/config.ini');
 
-//$mrs = new memcache\Stats();
-//$mrs->run('./user_agent_curl.php');
-//$mrs->menu();
+// $mrs = new memcache\Stats();
+// $mrs->run('./user_agent_curl.php');
+// $mrs->menu();
 
 $rm = new localCli\StatsMenu($argv, $argc);
 $rm->readLine();
