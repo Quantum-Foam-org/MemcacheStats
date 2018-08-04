eb7f41f249b17d3a259e14a3e455c1003991f5c7
Michael Alaimo <malaimo923@gmail.com>
2018-08-03 22:32:37 -0400

    Remove comment

--- Stats.php
+++ Stats.php
@@ -1,13 +1,13 @@
 <?php
-$common_php_dir = 'php_common';
+$common_php_dir = '../php_common';
 $common_autoload_file = $common_php_dir . '/autoload.php';
 require ($common_autoload_file);
 
-$cli_php_dir = 'php_cli';
+$cli_php_dir = '../php_cli';
 $cli_autoload_file = $cli_php_dir . '/autoload.php';
 require ($cli_autoload_file);
 
-require ('local/autoload.php');
+require ('./local/autoload.php');
 
 use cli\classes as cli;
 use local\classes\cli as localCli;
